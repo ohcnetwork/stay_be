99650718f6ef94ce38e679e7e7dc676302ece6bb
Abhiram Pai <abhirampai1999@gmail.com>
2020-04-11 13:18:03 +0530

    Add files via upload

--- src/stay/stay.module.ts
+++ src/stay/stay.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { StayController } from './stay.controller';
 import { StayService } from './stay.service';
+import { StayRepository } from './stay.repository'; 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Stay } from './entities/Stay.entity';
+import * as config from 'config';
+
 @Module({
+    imports:[
+        TypeOrmModule.forFeature([Stay, StayRepository]),
+    ],
     controllers:[StayController],
     providers:[StayService],
+    exports:[StayService]
 })
 export class StayModule {}
