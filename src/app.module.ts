bbf2f0bc16a1f92b8c55cb3e8ecdf3e9a87c8f41
Abhiram Pai <abhirampai1999@gmail.com>
2020-04-11 11:00:22 +0530

    imported stay controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,16 @@ import { AuthModule } from './auth/auth.module';
 import {DbConfig} from './config/db.config'
 import {TypeOrmModule} from '@nestjs/typeorm';
 import { AuthController } from './auth/auth.controller';
+import { StayController } from './stay/stay.controller';
+import { StayModule } from './stay/stay.module';
 
 @Module({
   imports: [
     AuthModule,
+    StayController,
     TypeOrmModule.forRoot(DbConfig),
   ],
-  controllers: [AuthController],
+  controllers: [AuthController,StayController],
   providers: [],
 })
 export class AppModule {}
