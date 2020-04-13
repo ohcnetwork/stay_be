ab4874775ce4e302e3657717e4ee6faa7389ecae
Abhiram Pai <abhirampai1999@gmail.com>
2020-04-13 19:53:52 +0530

    added update facility

--- src/facility/facility.controller.ts
+++ src/facility/facility.controller.ts
@@ -1,7 +1,7 @@
 import { Body,Controller,Post, Logger, Get, Put,Request } from '@nestjs/common';
 import { FacilityService } from './facility.service';
 import { ApiUseTags } from '@nestjs/swagger';
-import { AddFacilityDto,UpdateRoomDto } from './dto';
+import { AddFacilityDto,UpdateRoomDto,UpdateFacilityDto } from './dto';
 
 @ApiUseTags('Facility Management')
 @Controller('api/v1/facility')
@@ -21,23 +21,28 @@ export class FacilityController {
         return this.facilityService.getAllStay(req);
     }
 
+    @Get("users-facility")
+    getFacility(@Request() req:any){
+        this.logger.verbose('retrieving faclility of the user');
+        return this.facilityService.getFacility(req.user);
+    }
 
     @Post('add-facility')
     addfacility(@Body() addfacilityDto: AddFacilityDto) {
         this.logger.verbose("facility created");
         return this.facilityService.addfacility(addfacilityDto);
     }
-    
-/*    @Put('update-Roomavailability')
-    updateRooms(@Request() req: any,@Body() updateRoomDto: UpdateRoomDto) {
-        this.logger.verbose("room availability updated");
-        return this.facilityService.updateRooms(req.facility,updateRoomDto);
-    }*/
 
-    @Post('deleteFacility')
+    @Post('delete-Facility')
     deleteFacility(@Request() req: any,@Body() updateRoomDto: UpdateRoomDto) {
         this.logger.verbose("facility removed");
-        return this.facilityService.deleteFacility(req.facility,updateRoomDto);
+        return this.facilityService.deleteFacility(req.facility,updateRoomDto);//updateRoomDto added only for testing purpose
+    }
+
+    @Put('update-Facility')
+    updateFacility(@Request() req: any,@Body() updateFacilityDto: UpdateFacilityDto) {
+        this.logger.verbose("facility updated");
+        return this.facilityService.updateFacility(req.facility,updateFacilityDto);
     }
 
 
