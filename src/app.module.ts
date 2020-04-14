23f77f0962e79f28258cdd0034d355305e836517
Abhiram Pai <abhirampai1999@gmail.com>
2020-04-14 18:53:15 +0530

    added Rooms Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,17 @@ import {TypeOrmModule} from '@nestjs/typeorm';
 import { AuthController } from './auth/auth.controller';
 import { FacilityController } from './facility/facility.controller';
 import { FacilityModule } from './facility/facility.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { RoomsController } from './rooms/rooms.controller';
 
 @Module({
   imports: [
     AuthModule,
     FacilityModule,
+    RoomsModule,
     TypeOrmModule.forRoot(DbConfig),
   ],
-  controllers: [AuthController,FacilityController],
+  controllers: [AuthController,FacilityController,RoomsController],
   providers: [],
 })
 export class AppModule {}
