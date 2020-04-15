0ede7de304d6384c090a8c3d770b8d33a014b821
syamkumar <syam@Mozilla-community.org>
2020-04-16 03:29:36 +0530

    sentry added

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import * as config from 'config';
 import { join } from 'path';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as Sentry from '@sentry/node';
+
+Sentry.init({
+  dsn: 'https://1ece66a620394c65af3e1dcc783ecd78@o371117.ingest.sentry.io/5201888' }
+  );
 
 async function bootstrap() {
   const logger = new Logger('Coronasafe Root');
