213c2a690235398fd363190b4e7721a938f7f43c
Abhiram Pai <abhirampai1999@gmail.com>
2020-04-16 22:19:19 +0530

    Add files via upload

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import {DbConfig} from './config/db.config'
+import {TypeOrmModule} from '@nestjs/typeorm';
+import { AuthController } from './auth/auth.controller';
+import { FacilityController } from './facility/facility.controller';
+import { FacilityModule } from './facility/facility.module';
+import { RoomsModule } from './rooms/rooms.module';
+import { RoomsController } from './rooms/rooms.controller';
+import { RoleController } from './role/role.controller';
+import { RoleModule } from './role/role.module';
+import { BookingController } from './booking/booking.controller';
+import { BookingModule } from './booking/booking.module';
+
+
+@Module({
+  imports: [
+    AuthModule,
+    FacilityModule,
+    RoomsModule,
+    BookingModule,
+    TypeOrmModule.forRoot(DbConfig),
+    RoleModule,
+  ],
+  controllers: [AuthController,FacilityController,RoomsController, BookingController, RoleController],
+  providers: [],
+})
+export class AppModule {}
