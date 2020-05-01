be97ccefeecf389ce85449903bb356c0b6f59deb
Bodhisha Thomas <bodhisha@gmail.com>
2020-05-01 20:10:43 +0530

    Update local.strategy.ts

--- src/auth/strategy/local.strategy.ts
+++ src/auth/strategy/local.strategy.ts
@@ -14,7 +14,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(email: string, password: string): Promise<any> { 
     const user = await this.authService.validateUser(email, password);
     if (!user) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException({detail:'No active account found with the given credentials', code :'no_active_account'});
     }
     return user;
   }
