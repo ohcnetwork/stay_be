c79e308d6e6138171abdf3d3316ad68eb510699a
amalaabraham3 <amalaabraham3@gmail.com>
2020-05-02 23:35:35 +0530

    added roomnumber to booking table

--- src/booking/booking.repository.ts
+++ src/booking/booking.repository.ts
@@ -198,8 +198,8 @@ export class BookingRepository extends Repository<Booking> {
              .innerJoin('bookings.room','room')
              .innerJoin('room.facility','facility')
              .innerJoin('bookings.guestdetail','guestdetail')
-             .select(['bookings.book_id','bookings.checkin','bookings.checkout','bookings.statusBooking','bookings.statusCheckin',
-                    'bookings.createdAt','bookings.updatedAt', 'room.category','room.cost','user.name','user.email','guestdetail.name','guestdetail.age','guestdetail.gender','guestdetail.number'])
+             .select(['bookings.book_id','bookings.checkin','bookings.checkout','bookings.statusBooking','bookings.statusCheckin','bookings.roomNumber',
+                    'bookings.createdAt','bookings.updatedAt', 'room.category','room.cost','room.title','user.name','user.email','guestdetail.name','guestdetail.age','guestdetail.gender','guestdetail.number'])
              .where('facility.id = :id', {id:hotelId});
 
              if(await query.getCount()>0){
@@ -241,10 +241,14 @@ export class BookingRepository extends Repository<Booking> {
                     const book = await bookingRepository.findOne({book_id:id})
                     if (book.statusBooking === "BOOKED") {
                     book.statusCheckin=data.status;
+                    if(data.status==="CHECKEDIN"){
+                        book.roomNumber=data.roomNumber;
+                    }
                     book.save()
                     return {
                     status:true,
                     message:"Status Changed",
+                    data:book
                   }}
                   else {
                      throw new HttpException ( "Booking was cancelled!",HttpStatus.NOT_ACCEPTABLE);
