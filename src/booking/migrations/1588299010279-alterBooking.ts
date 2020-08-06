c637bfbef4640f7a83ff83c3c57e8d673e511c05
amalaabraham3 <amalaabraham3@gmail.com>
2020-08-06 14:47:10 +0530

    commented roomnumber & admin migration

--- src/booking/migrations/1588299010279-alterBooking.ts
+++ src/booking/migrations/1588299010279-alterBooking.ts
@@ -3,8 +3,8 @@ import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
 export class alterBooking1588299010279 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
-        const roomNumber = new TableColumn({name: 'roomNumber',type:'varchar',isNullable:true,default:null});
-        await queryRunner.addColumn("bookings",roomNumber);
+      //  const roomNumber = new TableColumn({name: 'roomNumber',type:'varchar',isNullable:true,default:null});
+       // await queryRunner.addColumn("bookings",roomNumber);
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
